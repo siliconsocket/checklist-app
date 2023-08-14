fc76dc8c551973c2c984c063c4d9839fef6faf74
Miguel Muñiz <mn.muniz@icloud.com>
2023-08-14 08:04:19 -0600

    feat[CustomInput]: add `onSave` props

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,38 +1,63 @@
-import { StyleSheet, Text, View, ViewStyle } from 'react-native'
-import React from 'react'
-import { TextInput } from 'react-native-paper'
+import { StyleSheet, Text, View, ViewStyle } from "react-native";
+import React from "react";
+import { Button, TextInput } from "react-native-paper";
 
 type TProps = {
-    placeholder?: string
-    label: string
-    value?: string
-    onChangeText?: (text:string) => void
-    containerStyle?: ViewStyle
-}
+  placeholder?: string;
+  label: string;
+  value?: string;
+  containerStyle?: ViewStyle;
+  onChangeText?: (text: string) => void;
+  onSave?: () => void;
+};
 
 const CustomInput = ({
-    placeholder = 'Ingresa una tarea...',
-    label = '',
-    value = '',
-    containerStyle = {},
-    onChangeText
-}:TProps) => {
+  placeholder = "Ingresa una tarea...",
+  label = "",
+  value = "",
+  containerStyle = {},
+  onChangeText,
+  onSave,
+}: TProps) => {
   return (
-    <View style={[styles.container,{...containerStyle}]}>
+    <View style={[styles.container, { ...containerStyle }]}>
       <TextInput
-      label={label}
-      placeholder={placeholder}
-      value={value}
-      onChangeText={(text) => onChangeText?.(text)}
-    />
+        label={label}
+        placeholder={placeholder}
+        value={value}
+        mode={"outlined"}
+        style={styles.input}
+        onChangeText={(text) => onChangeText?.(text)}
+      />
+      <Button
+        icon="content-save"
+        mode="contained"
+        style={styles.btn}
+        onPress={() => onSave?.()}
+      >
+        Guardar
+      </Button>
     </View>
-  )
-}
+  );
+};
 
-export default CustomInput
+export default CustomInput;
 
 const styles = StyleSheet.create({
-    container:{
-backgroundColor: '#FFF'
-    }
-})
+  container: {
+    backgroundColor: "#FFF",
+    paddingBottom: 10,
+    borderColor: "gray",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+  },
+  btn: {
+    marginLeft: 5,
+    width: 111,
+  },
+});
