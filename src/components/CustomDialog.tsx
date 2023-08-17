6b3da0f09f4bf59549099887b0fc9d832ea04cda
Miguel Muñiz <mn.muniz@icloud.com>
2023-08-17 08:24:25 -0600

    feat[CustomDialog]: add edit content with input text

--- src/components/CustomDialog.tsx
+++ src/components/CustomDialog.tsx
@@ -1,36 +1,61 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Dialog, Portal, Text, Button } from "react-native-paper";
+import React, { useState } from "react";
+import { Button, Dialog, Portal, Text } from "react-native-paper";
+import { DIALOG_TYPE } from "../../App";
+import CustomInput from "./CustomInput";
 
 type TDialogProps = {
   isVisible?: boolean;
+  title?: string;
+  dialogType: DIALOG_TYPE;
+  fieldValue?: string;
+  fieldLabel?: string;
   onDismiss?: () => void;
-  onConfirm?: () => void;
+  onConfirm?: (newName?: string) => void;
 };
 
 const CustomDialog = ({
   isVisible = false,
   onDismiss,
   onConfirm,
+  title = "Confirmación",
+  fieldValue = "",
+  fieldLabel = "Agregar tarea",
+  dialogType,
 }: TDialogProps) => {
+  const [newName, setNewName] = useState<string>(fieldValue);
+
+  const renderDialogContent = () => {
+    switch (dialogType) {
+      case DIALOG_TYPE.EDIT:
+        return (
+          <CustomInput
+            label={fieldLabel}
+            onChangeText={(text) => setNewName(text)}
+            value={newName}
+          />
+        );
+      case DIALOG_TYPE.CONFIRM:
+      default:
+        return (
+          <Text variant="bodyMedium">{"¿Deseas eliminar esta tarea?"}</Text>
+        );
+    }
+  };
+
   return (
     <Portal>
       <Dialog visible={isVisible} onDismiss={onDismiss}>
-        <Dialog.Title>{"Confirmación"}</Dialog.Title>
-        <Dialog.Content>
-          <Text variant="bodyMedium">{"¿Deseas eliminar esta tarea?"}</Text>
-        </Dialog.Content>
+        <Dialog.Title>{title}</Dialog.Title>
+        <Dialog.Content>{renderDialogContent()}</Dialog.Content>
         <Dialog.Actions>
-          <Button onPress={onDismiss}>Cancelar</Button>
-          <Button onPress={onConfirm}>Aceptar</Button>
+          {onDismiss && <Button onPress={onDismiss}>Cancelar</Button>}
+          {onConfirm && (
+            <Button onPress={() => onConfirm(newName)}>Aceptar</Button>
+          )}
         </Dialog.Actions>
       </Dialog>
     </Portal>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default CustomDialog;
